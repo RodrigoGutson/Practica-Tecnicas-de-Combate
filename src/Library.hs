0f958266f12b27c9b63f0083031a3a73299bf4b5
RodrigoGutson <rodrigutson@gmail.com>
2021-04-22 20:51:51 -0300

    Subo punto 3 + Tests correspondientes

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,42 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- Punto 1
+
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas / fortaleza objetivo
+
+poder :: Horas -> Number
+poder = (* 15)
+
+fortaleza :: Objetivo -> Number
+fortaleza = (2*) . length
+
+-- Punto 2
+
+type Golpe = Objetivo -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe 
+normalesConsecutivos = presionGolpe 240
+
+-- Punto 3 
+
+objetivoDificil :: Objetivo -> Bool 
+objetivoDificil = (< 100) . gomuGomu
+
+objetivoAccesible :: Objetivo -> Bool
+objetivoAccesible objetivo = (golpeFocalizado objetivo > 200) && (golpeFocalizado objetivo < 400)
+
+focalizar :: Objetivo -> Objetivo
+focalizar objetivo = take 7 objetivo
+
+golpeFocalizado :: Objetivo -> Number
+golpeFocalizado = normalesConsecutivos.focalizar
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,14 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+  describe "objetivoDificil" $ do
+    it "Objetivo es dificil" $ do
+      objetivoDificil "bolsa de entrenamiento" `shouldBe` True
+    it "Objetivo no es dificil" $ do
+      objetivoDificil "Puf" `shouldBe` False
+  describe "objetivoAccesible" $ do
+    it "Objetivo es accesible" $ do
+      objetivoAccesible "bolsa de entrenamiento" `shouldBe` True
+    it "Objetivo no es accesible" $ do
+      objetivoAccesible "Puf" `shouldBe` False
 
